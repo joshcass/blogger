ddeb61b27af5e1581db77793e6e59d85fa171aee
Josh Cass <joshuacass@gmail.com>
2015-06-03 20:10:37 -0600

    paperclipped

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603204616) do
+ActiveRecord::Schema.define(version: 20150604010722) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.text     "image_file_name"
+    t.text     "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
