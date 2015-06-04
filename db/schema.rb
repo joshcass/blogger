d728a10abba33a1791e6df661b8614b3f1bf496c
Josh Cass <joshuacass@gmail.com>
2015-06-03 21:09:10 -0600

    authentication mostly done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604010722) do
+ActiveRecord::Schema.define(version: 20150604024514) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "title"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20150604010722) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "username"
+  end
+
+  add_index "authors", ["email"], name: "index_authors_on_email", unique: true
 
   create_table "comments", force: :cascade do |t|
     t.text     "author_name"
