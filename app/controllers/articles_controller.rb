a5b5614c4d2deb6f9a73679e0232f0659e5fd953
Josh Cass <joshuacass@gmail.com>
2015-06-04 09:01:19 -0600

    rss feed?

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,4 +44,11 @@ class ArticlesController < ApplicationController
       raise "error"
     end
   end
+
+  def feed
+   @articles = Article.all
+    respond_to do |format|
+      format.rss {render layout: false}
+    end
+  end
 end
